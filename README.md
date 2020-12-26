4032e87505cd10f9774efd0223dd67a78687c323
Andy Liang <andyzen619@gmail.com>
2020-12-26 15:16:54 -0500

    Initialize template-firebase

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Tailwind template with firebase
+
+## Usage
+
+1. Make .env from filling out .env.example
+2. Run ```yarn install``` to install all required packages.
+3. Run ```yarn start``` to run app.
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './components/App/App';
+import reportWebVitals from './reportWebVitals';
+import firebase from './firebase';
+import FirebaseContext from './context/FirebaseContext';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <FirebaseContext.Provider value={firebase}>
+        <App />
+      </FirebaseContext.Provider>
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

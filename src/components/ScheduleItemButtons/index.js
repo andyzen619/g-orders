01852ed3e3b76443a1f7e75362931cb10b606866
Andy <andyzen619@gmail.com>
2020-12-30 13:40:47 -0500

    - update Schedule Buttons

--- src/components/ScheduleItemButtons/index.js
+++ src/components/ScheduleItemButtons/index.js
@@ -24,11 +24,12 @@ const ScheduleItemButtons = ({id}) => {
   return (
     <div className='flex justify-center'>
       <Button text='Edit' color='blue'/>
-      <Button text='Delete' color='red' onClick={() => {
-        const newOrdersOfTheDay = [...ordersOfTheDay]
-            .filter(({id: uid}) => uid !== id);
-        setOrdersOfTheDay(newOrdersOfTheDay);
-      }}/>
+      <Button text='Delete' color='red'
+        onClick={() => {
+          const newOrdersOfTheDay = [...ordersOfTheDay]
+              .filter(({id: uid}) => uid !== id);
+          setOrdersOfTheDay(newOrdersOfTheDay);
+        }}/>
     </div>
   );
 };

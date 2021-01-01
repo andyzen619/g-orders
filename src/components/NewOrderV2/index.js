7f92bf771051d6fbcf7ba3d450f3dd6292e2c505
Andy <andyzen619@gmail.com>
2021-01-01 15:43:20 -0500

    - update new order v2 componenet

--- src/components/NewOrderV2/index.js
+++ src/components/NewOrderV2/index.js
@@ -1,7 +1,12 @@
-import React, {useContext, useState} from 'react';
+/* eslint-disable valid-jsdoc */
+/* eslint-disable max-len */
+import React, {useContext, useState, useEffect, useReducer} from 'react';
 import Fuse from 'fuse.js';
+import {useParams} from 'react-router-dom';
 
 import {NewOrderContext} from '../../context/NewOrderContext';
+// import {calculateOrder, calculateSize} from '../../utils';
+import orderReducer from '../../reducers/OrderReducer';
 import {ORDER_ACTION_TYPES} from '../../constants';
 
 const options = {
@@ -10,7 +15,14 @@ const options = {
   ],
 };
 
-const flattenMenuItems = (menuItems, pattern, greaterThanZero, order) => {
+/**
+ * Flattens menu items and in an array.
+ * @param {*} menuItems
+ * @param {*} pattern
+ * @param {*} greaterThanZero
+ * @param {*} order
+ */
+export const flattenMenuItems = (menuItems, pattern, greaterThanZero, order) => {
   if (!menuItems) return [];
   if (!order) return [];
 
@@ -42,20 +54,32 @@ const flattenMenuItems = (menuItems, pattern, greaterThanZero, order) => {
 };
 
 const NewOrderV2 = () => {
-  const {menuItems, order, orderDispatch} = useContext(NewOrderContext);
+  const {menuItems} = useContext(NewOrderContext);
   const [search, setSearch] = useState('');
   const [greaterThanZero, setGreaterThanZero] = useState(false);
+  // const [order, setOrder] = useState({total: '0.00', time: '', size: '', phoneNumber: ''});
+  const [order, orderDispatch] = useReducer(orderReducer, {total: '0.00', time: '', size: '', phoneNumber: ''});
+
+  const {id} = useParams();
+
+  useEffect(() => {
+    if (!id) orderDispatch({type: ORDER_ACTION_TYPES.CLEAR_ORDER});
+  }, []);
+
+  // const clearOrder = () => {};
+  // const addItem = () => {};
+  // const removeItem = () => {};
 
   return (
     <div className='flex flex-col h-screen'>
-      <div className='flex justify-evenly bg-gray-300 p-4'>
-        <div>Search: </div>
-        <div>
+      <div className='flex justify-evenly bg-gray-500 p-4'>
+        <div className='text-white text-lg font-bold'>Search: </div>
+        <div className='mx-2'>
           <input onChange={(e) => setSearch(e.target.value)}/>
           <button
-            className='p-1 bg-gray-500 text-white'
+            className='p-1 bg-white mt-4 rounded-md text-2xl text-gray-500'
             onClick={() => setGreaterThanZero(!greaterThanZero)}
-          >Order</button>
+          >Current Order</button>
         </div>
       </div>
       <div className='overflow-y-auto h-full'>
@@ -68,10 +92,12 @@ const NewOrderV2 = () => {
               <div>{menuItem.price}</div>
             </div>
             <div className='flex'>
-              <div className='flex justify-between my-8 mx-2 w-8'>
+              <div className='flex justify-between my-8 mx-2 w-36'>
                 <div
+                  className='bg-green-300 px-6 py-1 rounded-lg'
                   onClick={
                     () => {
+                      console.log('adding item');
                       orderDispatch({
                         type: ORDER_ACTION_TYPES.ADD_ITEM,
                         item: menuItem,
@@ -79,8 +105,10 @@ const NewOrderV2 = () => {
                     }}
                 >+</div>
                 <div
+                  className='bg-red-300 px-6 py-1 rounded-lg'
                   onClick={
                     () => {
+                      console.log('removing item');
                       orderDispatch({
                         type: ORDER_ACTION_TYPES.REMOVE_ITEM,
                         toRemove: menuItem.name,
@@ -96,13 +124,22 @@ const NewOrderV2 = () => {
           </div>
         ))}
       </div>
-      <div className='flex'>
-        <div>SubTotal: </div>
-        <div>{order.total}</div>
-      </div>
-      <div className='flex'>
-        <div>Total: </div>
-        <div>{Number(order.total) * 1.13}</div>
+      <div className='flex justify-between bg-gray-500 text-white font-bold text-lg'>
+        <div className='flex flex-col'>
+          <div className='flex p-2'>
+            <div>SubTotal: </div>
+            <div>{order.total}</div>
+          </div>
+          <div className='flex p-2 text-4xl font-bold'>
+            <div>Total: </div>
+            <div>{(Number(order.total) * 1.13).toFixed(2)}</div>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+
+          }}
+          className='m-8 bg-white text-gray-500 rounded-md p-1 text-xl '> Submit</button>
       </div>
     </div>
   );

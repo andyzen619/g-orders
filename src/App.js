3978b05793368c466b44541b18406116203e8734
Andy <andyzen619@gmail.com>
2021-05-24 18:42:17 -0400

    - refactor folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {QueryClient, QueryClientProvider} from 'react-query';
-import Router from './router';
+import Router from './routes';
 
 const queryClient = new QueryClient();
 

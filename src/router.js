169218b5bd9c3ed1ac2100e1e5e65e968c9bad36
Andy <andyzen619@gmail.com>
2021-05-24 12:35:52 -0400

    - remove all code and prepare for V2

--- src/router.js
+++ src/router.js
@@ -2,22 +2,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import Home from './views/Home';
-import NewOrder from './views/NewOrder';
-
 const router = () => {
   return (
 
     <Router>
       <Switch>
-        <Route path='/editOrder/:id'>
-          <NewOrder/>
-        </Route>
-        <Route path='/newOrder/:startDate'>
-          <NewOrder/>
-        </Route>
         <Route path='/'>
-          <Home/>
+          <div>Home</div>
         </Route>
       </Switch>
     </Router>

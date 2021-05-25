04138cd2392b97d194d4c6968441ebfb68f4ccad
Andy <andyzen619@gmail.com>
2021-05-25 00:38:48 -0400

    - create button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Button = (props) => {
   const { buttonText } = props;
   return (
-    <div className="bg-gray-600 text-gray-800 font-semibold h-14 w-14 border border-gray-600 rounded-full shadow flex justify-center">
+    <div className="bg-gray-600 text-gray-800 font-semibold text-sm h-14 w-14 border border-gray-600 rounded-full shadow flex justify-center">
       <div className="flex flex-col justify-center text-white">{buttonText}</div>
     </div>
   );
